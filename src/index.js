fd463f8251bcf7fc307914e51d5b54631af864e3
jasonejavier <jjavier2097@gmail.com>
2018-07-27 14:27:36 +0800

    NEW INDEX

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 import App from './containers/App';
 import store from './store';
-//HAHA
+//HAHAHAHA
 const element = (
     <BrowserRouter>
         <Provider store={store}>
